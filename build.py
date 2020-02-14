0451780f7d0203d6b1c27c0c5484c23b9fdeab5f
MysticNebula70 <alpha5130@outlook.com>
2020-02-14 15:19:38 +0800

    Make consistency

--- build.py
+++ build.py
@@ -12,7 +12,7 @@ def build_pack(type='normal'):
         zipped_pack.write("pack.mcmeta")
     elif type == 'compat':
         # Compatible build
-        zipped_pack = zipfile.ZipFile("mcwzh-meme_compatibility.zip", 'w')
+        zipped_pack = zipfile.ZipFile("mcwzh-meme_compatible.zip", 'w')
         zipped_pack.writestr("assets/minecraft/lang/zh_cn.json", json.dumps(lang_data, indent=4, ensure_ascii=True))
         # Processing mcmeta
         with open("pack.mcmeta", 'r', encoding='utf8') as meta:
