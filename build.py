091dd56da5a65512b7cec8c8834b645b0967951e
lakejason0 <sunliyuan200402@outlook.com>
2020-02-17 11:17:22 +0800

    Added automatic build: no figure

--- build.py
+++ build.py
@@ -10,6 +10,8 @@ def build_pack(type='normal'):
         zipped_pack = zipfile.ZipFile("mcwzh-meme.zip", 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=5)
         zipped_pack.writestr("assets/minecraft/lang/zh_meme.json", json.dumps(lang_data, indent=4, ensure_ascii=True))
         zipped_pack.write("pack.mcmeta")
+        zipped_pack.write("assets/minecraft/models/item/totem_of_undying.json")
+        zipped_pack.write("assets/minecraft/textures/entity/player.png")
     elif type == 'compat':
         # Compatible build
         zipped_pack = zipfile.ZipFile("mcwzh-meme_compatible.zip", 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=5)
@@ -19,20 +21,38 @@ def build_pack(type='normal'):
             metadata = json.load(meta)
         del metadata['language']
         zipped_pack.writestr("pack.mcmeta", json.dumps(metadata, indent=4, ensure_ascii=False))
+        zipped_pack.write("assets/minecraft/models/item/totem_of_undying.json")
+        zipped_pack.write("assets/minecraft/textures/entity/player.png")
+    elif type == 'no_figure':
+        # Normal build, without figure
+        zipped_pack = zipfile.ZipFile("mcwzh-meme_no_figure.zip", 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=5)
+        zipped_pack.writestr("assets/minecraft/lang/zh_meme.json", json.dumps(lang_data, indent=4, ensure_ascii=True))
+        zipped_pack.write("pack.mcmeta")
+    elif type == 'compat_no_figure':
+        # Compatible build, without figure
+        zipped_pack = zipfile.ZipFile("mcwzh-meme_compatible_no_figure.zip", 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=5)
+        zipped_pack.writestr("assets/minecraft/lang/zh_cn.json", json.dumps(lang_data, indent=4, ensure_ascii=True))
+        # Processing mcmeta
+        with open("pack.mcmeta", 'r', encoding='utf8') as meta:
+            metadata = json.load(meta)
+        del metadata['language']
+        zipped_pack.writestr("pack.mcmeta", json.dumps(metadata, indent=4, ensure_ascii=False))
     zipped_pack.write("LICENSE")
     zipped_pack.write("pack.png")
-    zipped_pack.write("assets/minecraft/models/item/totem_of_undying.json")
-    zipped_pack.write("assets/minecraft/textures/entity/player.png")
     zipped_pack.close()
 
 if len(sys.argv) > 1:
     if sys.argv[1] == "compat":
         build_pack("compat")
+        build_pack("compat_no_figure")
     elif sys.argv[1] == "all":
         build_pack()
         build_pack("compat")
+        build_pack("no_figure")
+        build_pack("compat_no_figure")
     elif sys.argv[1] == "normal":
         build_pack()
+        build_pack("no_figure")
 else:
     print("Needs 1 argument")
     exit(code=-1)
