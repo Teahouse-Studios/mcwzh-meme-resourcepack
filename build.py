cafe56075c342018c66b4a1cbeb49385ec43b22d
lakejason0 <sunliyuan200402@outlook.com>
2020-03-13 11:17:59 +0800

    Legacy? nope

--- build.py
+++ build.py
@@ -7,6 +7,7 @@ def main():
     parser = argparse.ArgumentParser(description="Automatically build resourcepacks")
     parser.add_argument('type', default='normal',help="Build type. This should be 'all', 'normal' or 'compat'.", choices=['all', 'normal', 'compat'])
     parser.add_argument('-n', '--without-figure', action='store_true', help="Do not add figures when building resource packs. If build type is 'all', this argument will be ignored.")
+    parser.add_argument('-l', '--legacy', action='store_true', help="(Not implemented) Use legacy format (.lang) when building resource packs. If build type is 'all', this argument will be ignored.")
     args = vars(parser.parse_args())
     if args['type'] == 'all':
         build_all()
@@ -32,6 +33,15 @@ def build(args):
         # normal build
         pack.writestr("assets/minecraft/lang/zh_meme.json", json.dumps(lang_data, indent=4, ensure_ascii=True))
         pack.write("pack.mcmeta")
+        # if not args['legacy']:
+            # normal build
+            # pack.writestr("assets/minecraft/lang/zh_meme.json", json.dumps(lang_data, indent=4, ensure_ascii=True))
+            # pack.write("pack.mcmeta")
+        # else
+            # legacy build
+            # legacy_lang_data = ""
+            # for k, v in lang_data.items():
+            #     legacy_lang_data += "%s=%s\n" %(key,val)
     elif args['type'] == 'compat':
         # compatible build
         pack.writestr("assets/minecraft/lang/zh_cn.json", json.dumps(lang_data, indent=4, ensure_ascii=True))
@@ -43,20 +53,25 @@ def build(args):
     pack.close()
 
 def build_all():
-    build({'type': 'normal', 'without_figure': False})
-    build({'type': 'normal', 'without_figure': True})
-    build({'type': 'compat', 'without_figure': False})
-    build({'type': 'compat', 'without_figure': True})
+    build({'type': 'normal', 'without_figure': False, 'legacy': False})
+    build({'type': 'normal', 'without_figure': True, 'legacy': False})
+    build({'type': 'compat', 'without_figure': False, 'legacy': False})
+    build({'type': 'compat', 'without_figure': True, 'legacy': False})
+#    build({'type': 'normal', 'without_figure': False, 'legacy': True})
+#    build({'type': 'normal', 'without_figure': True, 'legacy': True})
+#    build({'type': 'compat', 'without_figure': False, 'legacy': True})
+#    build({'type': 'compat', 'without_figure': True, 'legacy': True})
 
 def get_packname(args):
     base_name = "mcwzh-meme"
     if args['type'] == 'normal':
-        if args['without_figure']:
-            base_name = base_name + "_nofigure"
+        pass
     elif args['type'] == 'compat':
         base_name = base_name + "_compatible"
-        if args['without_figure']:
-            base_name = base_name + "_nofigure"
+    if args['without_figure']:
+        base_name = base_name + "_nofigure"
+    if args['legacy']:
+        base_name = base_name + '_legacy'
     return base_name + ".zip"
 
 if __name__ == '__main__':
