4580842988eb1be7294ec43d6f0ce8cd45ed6a00
lakejason0 <36039861+lakejason0@users.noreply.github.com>
2020-03-31 15:40:59 +0800

    New model (#49)
    * new-model
    
    * Merge (#46)
    
    * Update
    
    * Update
    
    * Update build.py and missing files
    
    * 更改了顺序
    
    * 更新了顺序
    
    * 優化模型
    
    * 空的雕塑現在是Steve皮膚了
    
    * Merge (#48)
    
    * Update
    
    * Update
    
    * Create skyeyefast.png (#47)
    
    Co-authored-by: SkyEye_FAST <61134881+SkyEye-FAST@users.noreply.github.com>
    
    * Move file
    
    * Update model
    
    * Update blockstate
    
    * Update structure
    
    * Update structure
    
    * Update
    
    * Update
    
    * Suitable for work/live
    
    * sfw
    
    * Update
    
    * Update
    
    * Inappropriate content removal
    
    Co-authored-by: Gu_ZT <administration@ztxy666.cn>
    Co-authored-by: SkyEye_FAST <61134881+SkyEye-FAST@users.noreply.github.com>

--- build.py
+++ build.py
@@ -2,6 +2,8 @@ import zipfile
 import json
 import argparse
 import os
+import warnings
+from pathlib import Path
 
 mappings = ['addServer', 'advancements', 'advMode', 'attribute', 'book', 'build', 'chat',
             'commands', 'connect', 'container', 'controls', 'createWorld', 'death',
@@ -24,12 +26,14 @@ def main():
         description="Automatically build resourcepacks")
     parser.add_argument('type', default='normal', help="Build type. This should be 'all', 'normal' or 'compat'.", choices=[
                         'all', 'normal', 'compat'])
-    parser.add_argument('-n', '--without-figure', action='store_true',
-                        help="Do not use figure textures or models when building resource packs. If build type is 'all', this argument will be ignored.")
+    parser.add_argument('-t', '--figure', default='all', choices=['all', 'none'],
+                        help="Use or do not use figure textures or models when building resource packs. This should be 'all' or 'none'. For higher customizability, see '--mod-content'. If build type is 'all', this argument will be ignored.")
+    parser.add_argument('-s', '--sfw', action='store_true',
+                        help="Use 'suitable for work' strings. If build type is 'all', this argument will be ignored.")
     parser.add_argument('-l', '--legacy', action='store_true',
                         help="(Not fully implemented) Use legacy format (.lang) when building resource packs. If build type is 'all', this argument will be ignored.")
-    parser.add_argument('-m', '--mod-content', type=str, nargs='*',
-                        help="(Not fully implemented) Include modification strings. Should be path(s) to a file or 'all'. If build type is 'all', this argument will be ignored.")
+    parser.add_argument('-i', '-m', '--include', type=str, nargs='*',
+                        help="(Not fully implemented) Include modification strings or folders. Should be path(s) to a file, folder or 'all'. If build type is 'all', this argument will be ignored.")
     parser.add_argument('-d', '--debug', action='store_true',
                         help="Output an individual language file. If build type is 'all', this argument will be ignored.")
     args = vars(parser.parse_args())
@@ -45,6 +49,8 @@ def build(args):
     global pack_counter
     global successful_pack_counter
     global warning_pack_counter
+    if not args['debug']:
+        warnings.filterwarnings("ignore")
     with open("assets/minecraft/lang/zh_meme.json", 'r', encoding='utf8') as f:
         lang_data = json.load(f)
     pack_name = get_packname(args)
@@ -55,21 +61,32 @@ def build(args):
         pack_name, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=5)
     pack.write("pack.png")
     pack.write("LICENSE")
+    # build without figures
+    if args['figure'] == 'none':
+        exclude_list = ['optional/brewing_stand_model',
+                        'optional/totem_model', 'optional/observer_think']
+        for i in exclude_list:
+            for i in args['include']:
+                args['include'].remove(i)
     # build with figures
-    if not args['without_figure']:
-        for file in os.listdir("assets/minecraft/models/item"):
-            pack.write("assets/minecraft/models/item/" + file)
-        for file in os.listdir("assets/minecraft/textures/entity"):
-            pack.write("assets/minecraft/textures/entity/" + file)
-        for file in os.listdir("assets/minecraft/textures/block"):
-            pack.write("assets/minecraft/textures/block/" + file)
+    if args['figure'] == 'all':
+        include_list = ['optional/brewing_stand_model',
+                        'optional/totem_model', 'optional/observer_think']
+        for i in include_list:
+            if i not in args['include']:
+                args['include'].append(i)
+    # build with sfw
+    if args['sfw']:
+        args['include'].append('optional/sfw.json')
     # build with mod content
     moddata = {}
-    if args['mod_content']:
-        if 'all' in args['mod_content']:
+    if args['include']:
+        if 'all' in args['include']:
             modlist = ['mods/' + i for i in os.listdir("mods")]
+            modlist.extend(args['include'])
+            modlist.remove('all')
         else:
-            modlist = args['mod_content']
+            modlist = args['include']
         for file in modlist:
             if os.path.isfile(file):
                 if file.endswith(".json"):
@@ -84,9 +101,13 @@ def build(args):
                     print(
                         '\033[33m[WARN] File extension "%s" is not supported, skipping\033[0m' % file[file.rfind('.') + 1:])
                     warning_counter += 1
+            elif Path(file).is_dir():
+                for item in Path(file).rglob('*'):
+                    pack.write(item, str(item.relative_to(
+                        '.').as_posix()).split(file, 1)[-1])
             else:
                 print(
-                    '\033[33m[WARN] File "%s" does not exist, skipping\033[0m' % file)
+                    '\033[33m[WARN] File or folder "%s" does not exist, skipping\033[0m' % file)
                 warning_counter += 1
         lang_data.update(moddata)
     # Processing mcmeta
@@ -131,7 +152,7 @@ def build(args):
                     else:
                         legacy_lang_data.update({k: lang_data[v]})
         # build with mod content
-        if args["mod_content"]:
+        if args["include"]:
             legacy_lang_data.update(moddata)
         legacy_lang_file = ""
         for k, v in legacy_lang_data.items():
@@ -156,34 +177,31 @@ def build(args):
 
 
 def build_all():
-    build({'type': 'normal', 'without_figure': False,
-           'legacy': False, 'mod_content': ['all'], 'debug': False})
-    build({'type': 'normal', 'without_figure': True,
-           'legacy': False, 'mod_content': ['all'], 'debug': False})
-    build({'type': 'compat', 'without_figure': False,
-           'legacy': False, 'mod_content': [], 'debug': False})
-    build({'type': 'compat', 'without_figure': True,
-           'legacy': False, 'mod_content': [], 'debug': False})
-#    build({'type': 'normal', 'without_figure': False, 'legacy': True, 'debug': False})
-#    build({'type': 'normal', 'without_figure': True, 'legacy': True, 'debug': False})
-#    build({'type': 'compat', 'without_figure': False, 'legacy': True, 'debug': False})
-    build({'type': 'compat', 'without_figure': True,
-           'legacy': True, 'mod_content': [], 'debug': False})
+    build({'type': 'normal', 'figure': 'all',
+           'legacy': False, 'include': ['all'], 'debug': False})
+    build({'type': 'normal', 'figure': 'none',
+           'legacy': False, 'include': ['all'], 'debug': False})
+    build({'type': 'compat', 'figure': 'all',
+           'legacy': False, 'include': [], 'debug': False})
+    build({'type': 'compat', 'figure': 'none',
+           'legacy': False, 'include': [], 'debug': False})
+    build({'type': 'compat', 'figure': 'none',
+           'legacy': True, 'include': [], 'debug': False})
 
 
 def get_packname(args):
     base_name = "mcwzh-meme"
     if args['type'] == 'normal':
-        if not args['mod_content']:
+        if not args['include']:
             base_name += "_nomod"
     elif args['type'] == 'compat':
         base_name += "_compatible"
-        if args['mod_content']:
-            base_name += "_mod"
-    if args['without_figure']:
+    if args['figure'] == 'none':
         base_name += "_nofigure"
     if args['legacy']:
         base_name += '_legacy'
+    if 'optional/sfw.json' in args['include']:
+        base_name += '_sfw'
     return base_name + ".zip"
 
 
