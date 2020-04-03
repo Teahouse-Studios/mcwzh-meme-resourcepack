7d5f2964f8a4639077bb72b815f6983d774f5ca1
lakejason0 <sunliyuan200402@outlook.com>
2020-04-03 14:28:41 +0800

    Update

--- build_online.py
+++ build_online.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import build
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./', static_folder="", static_url_path="")
 
 @app.route('/')
 @app.route('/index')
@@ -11,13 +11,15 @@ def generate_website():
     enmods = set()
     optionals = set()
     mods.update(["mods/" + file for file in os.listdir('mods')])
-    enmods.update(["mods/" + file for file in os.listdir('en-mods')])
+    enmods.update(["en-mods/" + file for file in os.listdir('en-mods')])
     optionals.update(["optional/" + file for file in os.listdir('optional')])
-    return render_template("index.html")
+    return render_template("./index.html", mods = mods, enmods = enmods, optionals = optionals)
 
 @app.route('/ajax',methods=['POST'])
 def ajax():
-    pass
+    recv_data = request.get_data()
+    print(recv_data)
+    return recv_data
 
 if __name__ == '__main__':
     app.run(debug=True)
