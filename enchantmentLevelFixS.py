3c129394c80ac96e43100fe435c6e309d022af6c
lakejason0 <sunliyuan200402@outlook.com>
2020-05-04 17:56:48 +0800

    Update

--- enchantmentLevelFixS.py
+++ enchantmentLevelFixS.py
@@ -6,10 +6,10 @@ lang = {}
 
 i = 11
 
-while i <= 2147483647:
+while i <= 32767:
     lang['enchantment.level.' + str(i)] = str(i)
     print(i)
     i += 1
 
-with open("enchlevelfix.json", 'w', encoding='utf8') as lang_file:
+with open("enchlevelfixS.json", 'w', encoding='utf8') as lang_file:
     lang_file.write(json.dumps(lang, indent=4, ensure_ascii=False))
