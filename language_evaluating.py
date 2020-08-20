b3bf3c8e12f7e972ff78072463a4bd1fab9c5500
lakejason0 <sunliyuan200402@outlook.com>
2020-08-20 09:43:25 +0800

    Update missing strings

---
+++ language_evaluating.py
@@ -0,0 +1,11 @@
+import os
+import json
+# It's a garbage
+
+with open("zh_cn.json",'r',encoding='utf8') as f:
+    official_lang_data = json.load(f)
+
+with open("assets/minecraft/lang/zh_meme.json",'r',encoding='utf8') as f:
+    meme_lang_data = json.load(f)
+
+print(set(official_lang_data) ^ set(meme_lang_data))
