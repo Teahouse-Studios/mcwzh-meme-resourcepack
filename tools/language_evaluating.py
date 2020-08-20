f6fbfb41d4b6d151f67c9718b09140935b5f4842
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2020-08-20 10:21:59 +0800

    print improvement

--- tools/language_evaluating.py
+++ tools/language_evaluating.py
@@ -3,10 +3,10 @@ import json
 # It's a garbage
 # And somehow it's countable
 
-with open("zh_cn.json",'r',encoding='utf8') as f:
+with open("zh_cn.json", 'r', encoding='utf8') as f:
     official_lang_data = json.load(f)
 
-with open("assets/minecraft/lang/zh_meme.json",'r',encoding='utf8') as f:
+with open("assets/minecraft/lang/zh_meme.json", 'r', encoding='utf8') as f:
     meme_lang_data = json.load(f)
 
-print(set(official_lang_data) ^ set(meme_lang_data))
+print(*(set(official_lang_data) ^ set(meme_lang_data)), sep='\n')
