9fa3efcfeb5ca0c84861f337fba2e61151c3c5e2
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2020-09-09 21:11:45 +0800

    better use

--- tools/conversion.py
+++ tools/conversion.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, FileType
 from json import load, dump
 from re import match
 from sys import stdout
@@ -7,19 +7,19 @@ from sys import stdout
 def generate_parser() -> ArgumentParser:
     parser = ArgumentParser(
         description="Do S/T conversion by reading from two language files.")
-    parser.add_argument(
-        "json1", help="path to the first json. Would be the keys in the output.")
-    parser.add_argument(
-        "json2", help="path to the second json. Would be the values in the output.")
+    parser.add_argument("json1", type=FileType(mode='r', encoding='utf8'),
+                        help="path to the first json. Would be the keys in the output.")
+    parser.add_argument("json2", type=FileType(mode='r', encoding='utf8'),
+                        help="path to the second json. Would be the values in the output.")
     parser.add_argument("regex", help="specify the regex.")
-    parser.add_argument(
-        "--outfile", "-o", help="specify the name of sorted json. If omitted, will output the json to stdout.")
+    parser.add_argument("--outfile", "-o", nargs='?', default=stdout, type=FileType(mode='w', encoding='utf8'),
+                        help="specify the name of sorted json. If omitted, will output the json to stdout.")
     return parser
 
 
 def generate_conversion(args) -> dict:
-    json1 = load(open(args.json1, 'r', encoding='utf8'))
-    json2 = load(open(args.json2, 'r', encoding='utf8'))
+    json1 = load(args.json1)
+    json2 = load(args.json2)
     print(*(f'\033[33mWarning: "{k}" does not exist in the second json, skipping.\033[0m' for k in json1 if match(
         args.regex, k) and k not in json2), sep='\n')
     return dict((json1[k], json2[k]) for k in json1 if match(args.regex, k) and k in json2)
@@ -28,8 +28,4 @@ def generate_conversion(args) -> dict:
 if __name__ == '__main__':
     args = generate_parser().parse_args()
     out = generate_conversion(args)
-    if args.outfile:
-        dump(out, open(args.outfile, 'w', encoding='utf8'),
-             ensure_ascii=False, indent=4, sort_keys=True)
-    else:
-        dump(out, stdout, ensure_ascii=False, sort_keys=True)
+    dump(out, args.outfile, ensure_ascii=False, indent=4, sort_keys=True)
--- tools/enchantment_level.py
+++ tools/enchantment_level.py
@@ -5,17 +5,16 @@ from argparse import ArgumentParser
 
 
 def dump(max: int):
-    json.dump({f'enchantment.level.{i}': f'{i}' for i in range(11, max + 1)}, open("enchlevelfixS.json", 'w', encoding='utf8'),
-              indent=4, ensure_ascii=False)
+    json.dump({f'enchantment.level.{i}': f'{i}' for i in range(11, max + 1)},
+              open("enchlevelfixS.json", 'w', encoding='utf8'), indent=4, ensure_ascii=False)
 
 
 def generate_parser():
-    parser = ArgumentParser(
-        description="Generate enchantment level fix json.")
-    parser.add_argument('max', help='wanted max level.')
+    parser = ArgumentParser(description="Generate enchantment level fix json.")
+    parser.add_argument('max', type=int, help='wanted max level.')
     return parser
 
 
 if __name__ == '__main__':
     args = generate_parser().parse_args()
-    dump(int(args.max))
+    dump(args.max)
