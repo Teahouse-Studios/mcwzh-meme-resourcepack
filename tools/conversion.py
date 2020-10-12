4689453e99c103e6dc972bce85dd57fa180b34c5
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2020-10-12 18:52:47 +0800

    update tools

--- tools/conversion.py
+++ tools/conversion.py
@@ -4,28 +4,24 @@ from re import match
 from sys import stdout
 
 
-def generate_parser() -> ArgumentParser:
-    parser = ArgumentParser(
-        description="Do S/T conversion by reading from two language files.")
-    parser.add_argument("json1", type=FileType(mode='r', encoding='utf8'),
-                        help="path to the first json. Would be the keys in the output.")
-    parser.add_argument("json2", type=FileType(mode='r', encoding='utf8'),
-                        help="path to the second json. Would be the values in the output.")
-    parser.add_argument("regex", help="specify the regex.")
-    parser.add_argument("--outfile", "-o", nargs='?', default=stdout, type=FileType(mode='w', encoding='utf8'),
-                        help="specify the name of sorted json. If omitted, will output the json to stdout.")
-    return parser
-
-
-def generate_conversion(args) -> dict:
-    json1 = load(args.json1)
-    json2 = load(args.json2)
+def generate_conversion(json1, json2, regex) -> dict:
     print(*(f'\033[33mWarning: "{k}" does not exist in the second json, skipping.\033[0m' for k in json1 if match(
-        args.regex, k) and k not in json2), sep='\n')
+        regex, k) and k not in json2), sep='\n')
     return dict((json1[k], json2[k]) for k in json1 if match(args.regex, k) and k in json2)
 
 
 if __name__ == '__main__':
+    def generate_parser() -> ArgumentParser:
+        parser = ArgumentParser(
+            description="Do S/T conversion by reading from two language files.")
+        parser.add_argument("json1", type=FileType(mode='r', encoding='utf8'),
+                            help="path to the first json. Would be the keys in the output.")
+        parser.add_argument("json2", type=FileType(mode='r', encoding='utf8'),
+                            help="path to the second json. Would be the values in the output.")
+        parser.add_argument("regex", help="specify the regex.")
+        parser.add_argument("--outfile", "-o", nargs='?', default=stdout, type=FileType(mode='w', encoding='utf8'),
+                            help="specify the name of sorted json. If omitted, will output the json to stdout.")
+        return parser
     args = generate_parser().parse_args()
-    out = generate_conversion(args)
+    out = generate_conversion(load(args.json1), load(args.json2), args.regex)
     dump(out, args.outfile, ensure_ascii=False, indent=4, sort_keys=True)
--- tools/sort.py
+++ tools/sort.py
@@ -3,19 +3,17 @@ from json import load, dump
 from sys import stdout
 
 
-def sort(origin): return {k: origin[k] for k in sorted(list(origin))}
-
-
-def generate_parser() -> ArgumentParser:
-    parser = ArgumentParser(description="Sort a json by key.")
-    parser.add_argument("json", type=FileType(
-        mode='r', encoding='utf8'), help="path to the json.")
-    parser.add_argument("--outfile", "-o", nargs='?', default=stdout, type=FileType(mode='w', encoding='utf8'),
-                        help="specify the name of sorted json. If omitted, will output the json to stdout.")
-    return parser
+def sort(origin): return {k: origin[k] for k in sorted(origin)}
 
 
 if __name__ == '__main__':
+    def generate_parser() -> ArgumentParser:
+        parser = ArgumentParser(description="Sort a json by key.")
+        parser.add_argument("json", type=FileType(
+            mode='r', encoding='utf8'), help="path to the json.")
+        parser.add_argument("--outfile", "-o", nargs='?', default=stdout, type=FileType(mode='w', encoding='utf8'),
+                            help="specify the name of sorted json. If omitted, will output the json to stdout.")
+        return parser
     args = generate_parser().parse_args()
     dump(load(args.json), args.outfile,
          ensure_ascii=False, indent=4, sort_keys=True)
