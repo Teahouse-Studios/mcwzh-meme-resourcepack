76301cd6c1490c93e4c2f2151295b3e528c0cee1
wyapx <a1025952421@outlook.com>
2020-12-27 12:16:30 +0800

    Update lang2json.py

--- tools/lang2json.py
+++ tools/lang2json.py
@@ -1,30 +1,31 @@
-import json,re,os,sys,argparse
+import argparse
+import json
+import os
+import re
+import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument("infile",help="输入文件，编码必须为UTF-8")
-parser.add_argument("outfile",help="输出文件，编码为UTF-8")
-parser.add_argument("--indent","-i",help="格式化json",action="store_true")
-parser.add_argument("--reverse","-r",help="反转，即json转lang",action="store_true")
+parser.add_argument("infile", help="输入文件，编码必须为UTF-8")
+parser.add_argument("outfile", help="输出文件，编码为UTF-8")
+parser.add_argument("--indent", "-i", help="格式化json", type=int, default=2)
+parser.add_argument("--reverse", "-r", help="反转，即json转lang", action="store_true")
 args = parser.parse_args()
-if not os.path.exists(args.infile):
-	print(f"文件{args.infile}不存在！")
-	sys.exit(-1)
-f=open(args.infile,encoding='utf8')
-if args.reverse:
-	json=json.load(f)
-	f.close()
-	f=open(args.outfile,'w',encoding='utf8')
-	for key,value in json.items():
-		f.write(f"{key}={value}\n")
-	f.close()
-else:
-	obj={}
-	for i in f.readlines():
-		result=re.match("(.+)=(.+)",re.sub("#.*\n?","",i))
-		if not result:
-			continue
-		obj[result.group(1)]=result.group(2)
-	f.close()
-	f=open(args.outfile,'w',encoding="utf8")
-	json.dump(obj,f,ensure_ascii=False,indent=args.indent)
-	f.close()
+
+if not os.path.isfile(args.infile):
+    print(f"文件:", args.infile, "不存在！")
+    sys.exit(-1)
+
+with open(args.infile, encoding='utf8') as ifile:
+    if args.reverse:
+        json = json.load(ifile)
+        with open(args.outfile, 'w', encoding='utf8') as f:
+            for key, value in json.items():
+                f.write(f"{key}={value}\n")
+    else:
+        obj = {}
+        for i in ifile.readlines():
+            result = re.match("(.+)=(.+)", re.sub("#.*\n?", "", i))
+            if not result:
+                continue
+            obj[result.group(1)] = result.group(2)
+        json.dump(obj, open(args.outfile, 'w', encoding="utf8"), ensure_ascii=False, indent=args.indent)
