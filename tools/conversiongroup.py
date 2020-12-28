8fe3f80ffd171a815c8f02e2f3d4efda3f1ede71
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2020-12-28 21:07:42 +0800

    update tools

--- tools/conversiongroup.py
+++ tools/conversiongroup.py
@@ -1,23 +1,14 @@
-from io import TextIOWrapper
 from json import load, dump
 from re import match
 
 
-def generate_conversion_list(json1, json2, json3, regex) -> list:
-    print(*(f'\033[33mWarning: "{k}" does not exist in the second json, skipping.\033[0m' for k in json1 if match(
+def generate_conversion(json1, json2, json3, regex) -> list:
+    print(*(f'\033[33mWarning: "{k}" does not exist in the Traditional Chinese json, skipping.\033[0m' for k in json1 if match(
         regex, k) and k not in json2), sep='\n')
-    return list({'zh':json1[k], 'zh-cn':json1[k], 'zh-tw':json2[k], 'original': json3[k], 'description': k} for k in json1 if match(args.regex, k) and k in json2)
+    print(*(f'\033[33mWarning: "{k}" does not exist in the Original json, skipping.\033[0m' for k in json1 if match(
+        regex, k) and k not in json3), sep='\n')
+    return '\n'.join(f"{{type = 'item', original = '{json3[k]}', rule = 'zh:{json1[k]};zh-cn:{json1[k]};zh-tw:{json2[k]};', description = '{k}'}}," for k in json1 if match(regex, k) and k in json2 and k in json3)
 
-def generate_conversion_group(list) -> str:
-    a = ''
-    for k in list:
-        original = k['original']
-        zh = k['zh']
-        zh_cn = k['zh-cn']
-        zh_tw = k['zh-tw']
-        description = k['description']
-        a += f"{{type = 'item', original = '{original}', rule = 'zh:{zh};zh-cn:{zh_cn};zh-tw:{zh_tw};', description = '{description}'}},\n"
-    return a
 
 if __name__ == '__main__':
     from argparse import ArgumentParser, FileType
@@ -27,19 +18,16 @@ if __name__ == '__main__':
         parser = ArgumentParser(
             description="Do S/T conversion group modules by reading from three language files.")
         parser.add_argument("json1", type=FileType(mode='r', encoding='utf8'),
-                            help="path to the first json. Would be the S part in the output.")
+                            help="path to the Simplified Chinese json. Would be the S part in the output.")
         parser.add_argument("json2", type=FileType(mode='r', encoding='utf8'),
-                            help="path to the second json. Would be the T part in the output.")
-        parser.add_argument("json3", type=FileType(mode='r', encoding='utf8'), 
-                            help="path to the third json. Would be the original part in the output.")
+                            help="path to the Traditional Chinese json. Would be the T part in the output.")
+        parser.add_argument("json3", type=FileType(mode='r', encoding='utf8'),
+                            help="path to the Original json. Would be the original part in the output.")
         parser.add_argument("regex", help="specify the regex.")
-        parser.add_argument("--outfile", "-o", nargs='?', type=str,
-                            help="specify the name of sorted json. If omitted, will output the json to stdout.")
+        parser.add_argument("--outfile", "-o", nargs='?', default=stdout, type=FileType(mode='w', encoding='utf8'),
+                            help="specify the name of output json. If omitted, will output the json to stdout.")
         return parser
-        # default=stdout, type=FileType(mode='w', encoding='utf8'),
     args = generate_parser().parse_args()
-    out = generate_conversion_group(generate_conversion_list(load(args.json1), load(args.json2), load(args.json3), args.regex))
-    print(out)
-    # dump(out, args.outfile, ensure_ascii=False, indent=4, sort_keys=True)
-    with open(args.outfile, mode='w', encoding='utf8') as f:
-        f.write(out)
+    out = generate_conversion(load(args.json1), load(
+        args.json2), load(args.json3), args.regex)
+    dump(out, args.outfile, ensure_ascii=False, indent=4, sort_keys=True)
