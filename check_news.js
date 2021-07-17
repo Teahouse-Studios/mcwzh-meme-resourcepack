190f4b8a4df227917caf7006705724e19f889905
Dianliang233 <dianliang233@gmail.com>
2021-07-17 18:53:12 +0800

    Update CI

--- check_news.js
+++ check_news.js
@@ -1,5 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
-const content = fs.readFileSync(path.resolve(__dirname, 'news.json'))
-JSON.parse(content)
+const news = fs.readFileSync(path.resolve(__dirname, 'news.json'))
+JSON.parse(news)
+
+const alerts = fs.readFileSync(path.resolve(__dirname, 'alerts.json'))
+JSON.parse(alerts)
